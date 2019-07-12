dd7dd6f66737b518becca68403609e683e7454e4
U043RXL <RoseAnn.Logronio@carlsonwagonlit.com>
2019-07-12 21:59:11 +0800

    First commit

---
+++ src/main/java/GradleApp/App.java
@@ -0,0 +1,13 @@
+package GradleApp;
+
+public class App {
+
+	/**
+	 * Hello world!
+	 *
+	 */
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/GradleApp/AppTest.java
@@ -0,0 +1,8 @@
+package GradleApp;
+
+public class AppTest {
+
+	public static void main( String[] args ){
+	      System.out.println( "TEST Hello World!" );
+	   }
+}
